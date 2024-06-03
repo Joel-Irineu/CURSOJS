e3601f56bb14a87f24b3e1b3ae76142116280d2f
joel-irineu <irineu.joel01@gmail.com>
2024-06-03 13:37:47 -0300

    console.log

---
+++ Seção 02 - JS Básico - Para iniciantes/01 - console.log/index.js
@@ -0,0 +1,7 @@
+console.log('isso é um texto'); //string
+console.log("texto aspas duplas");
+console.log(`crases`);
+
+console.log(123456, 12345.12354); //number
+
+console.log(32, 15.85, "irineu");
