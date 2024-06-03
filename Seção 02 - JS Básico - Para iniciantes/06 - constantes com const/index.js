c0306d102541567a1cb3375dc18682d9173ad620
joel-irineu <irineu.joel01@gmail.com>
2024-06-03 15:01:43 -0300

    Constantes com const

---
+++ Seção 02 - JS Básico - Para iniciantes/06 - constantes com const/index.js
@@ -0,0 +1,9 @@
+// Não pode conter palavra reservada
+// Crie com nomes significativos
+// não pode começar com numero
+// não pode conter espacos ou traços
+// Case-sensitive
+// Não pode redeclarar variaveis com consts
+// Não podemos modificar o valor de uma const
+const nome = 'João';
+console.log(nome);
