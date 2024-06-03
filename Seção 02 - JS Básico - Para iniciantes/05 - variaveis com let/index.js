8c59ac912b578f27703adc08ffdcda40d8e93db5
joel-irineu <irineu.joel01@gmail.com>
2024-06-03 14:55:02 -0300

    Varsiaveis com let

---
+++ Seção 02 - JS Básico - Para iniciantes/05 - variaveis com let/index.js
@@ -0,0 +1,18 @@
+let nome; //declarou
+
+nome = 'João'; //inicializando valor
+console.log(nome);
+nome = 'Otávio'; // alterando valor
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000' ,nome, 'conheceu Maria.');
+console.log('Maria teve 1 filho com', nome, 'em 2015.');
+console.log('O filho de', nome, 'se chama Eduardo.');
+
+// Não pode conter palavra reservada
+// Crie com nomes significativos
+// não pode começar com numero
+// não pode conter espacos ou traços
+// Case-sensitive
+// Não pode redeclarar variaveis com LET
+// NÃO UTILIZE VAR, UTILIZE LET
