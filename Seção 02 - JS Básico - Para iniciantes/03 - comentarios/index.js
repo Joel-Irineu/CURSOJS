31fbb92fe3794926060a23c2805f67e28e308cf4
joel-irineu <irineu.joel01@gmail.com>
2024-06-03 13:52:51 -0300

    comentários

---
+++ Seção 02 - JS Básico - Para iniciantes/03 - comentarios/index.js
@@ -0,0 +1,16 @@
+// este é um comentário inline
+console.log("olá, mundo"); // outro comentário inline
+console.log("Linha 1");
+// comentário
+console.log("Linha 2");
+/**
+ * Um bloco de codgo
+ * linha de comentario
+ */
+console.log("Linha 3");
+
+// console.log("linha ignorada");
+
+console.log("Linha 4");
+
+// vai ser ignorado
